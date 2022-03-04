3d954f83432e908e668105cdd981d03e7e2d1a54
Alberto Sierra <alberto.sierra@murciaeduca.es>
2022-03-04 08:26:50 +0100

    Cambiando la descripción del plugin para que aparezca en la lista de plugins

--- CarlosIIIJobs/CarlosIIIJobs/CarlosIIIJobs.php
+++ CarlosIIIJobs/CarlosIIIJobs/CarlosIIIJobs.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              https://cifpcarlos3.es
  * @since             1.0.0
  * @package           CarlosIIIJobs
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/CarlosIIIJobs-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       CIFP Carlos III Jobs
+ * Plugin URI:        https://cifpcarlos3.es/CarlosIIIJobs-uri/
+ * Description:       Gestión de ofertas de trabajo para los alumnos del C.I.F.P. Carlos III
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            C.I.F.P. Carlos III
+ * Author URI:        https://cifpcarlos3.es
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       CarlosIIIJobs
